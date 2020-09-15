9f6bd976f3c36837693d83a4db654f2d6a3c86e2
Eric Feng <evf23@cornell.edu>
2020-09-14 23:03:27 -0400

    Updates

---
+++ precache-manifest.ffc6e844da003cf0891fac92cf47caf5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e8a0fe209b2e1069ef956611f49ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7694f2a6ff5f3cd1541",
+    "url": "/static/css/main.ce463544.chunk.css"
+  },
+  {
+    "revision": "3ebac0c447f696e2406f",
+    "url": "/static/js/2.fe539e1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe539e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7694f2a6ff5f3cd1541",
+    "url": "/static/js/main.92558ef8.chunk.js"
+  },
+  {
+    "revision": "848cef958fff589791f8",
+    "url": "/static/js/runtime-main.a4b448c3.js"
+  },
+  {
+    "revision": "37eb207e62917d14642a9509b0d955be",
+    "url": "/static/media/atlas.37eb207e.png"
+  },
+  {
+    "revision": "af06df00177d6f74b9491a771e7ef778",
+    "url": "/static/media/brutal60.af06df00.jpg"
+  },
+  {
+    "revision": "16a64f40c1efef8d191ad1a6c336fae5",
+    "url": "/static/media/kbd67gray.16a64f40.jpg"
+  },
+  {
+    "revision": "6e4c898e7106163661a6ffb30b2b6cd3",
+    "url": "/static/media/kbd67pc.6e4c898e.jpg"
+  },
+  {
+    "revision": "60551206b60534fc3cf82854a85dc7c7",
+    "url": "/static/media/leaf60.60551206.jpg"
+  },
+  {
+    "revision": "22b1918d351f554328eb53d61244cde6",
+    "url": "/static/media/me.22b1918d.png"
+  },
+  {
+    "revision": "2a2c8c6f75d801b6425bc4201a214521",
+    "url": "/static/media/nk65v2.2a2c8c6f.jpg"
+  },
+  {
+    "revision": "7b36e30e11abe076715b60b4df7797df",
+    "url": "/static/media/savage65.7b36e30e.jpg"
+  }
+]);
